f8840a9b6487466252419a3b09c1916b6938958d
Rafael Pivatto <rafael.pivatto@gmail.com>
2021-06-22 20:46:54 -0300

    aula 2

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,15 @@
 import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 
 export function NewRoom() {
+
+  const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
